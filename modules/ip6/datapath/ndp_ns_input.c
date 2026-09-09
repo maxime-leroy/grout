maxime-leroy/grout

commit b51dfd92f09fa43234519d132e955e0ba1bd09d0

ndp: avoid races between control and data planes
Do not duplicate neighbor solicitations packets to reply without going
through the control plane. This is bad design and can lead to races
between the control and data planes.

Send all valid NDP packets to control plane and if they are neighbor
solicitations, only *after* handling the request contents to update our
nexthop cache, send a neighbor advertisement for our requested local
address.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: David Marchand <david.marchand@redhat.com>

modules/ip6/datapath/meson.build
@@ -12,6 +12,7 @@ src += files(
   'ip6_local.c',
   'ip6_output.c',
   'ndp_na_input.c',
+  'ndp_na_output.c',
   'ndp_ns_input.c',
   'ndp_ns_output.c',
   'ndp_rs_input.c',

modules/ip6/datapath/ndp_ns_input.c
@@ -16,11 +16,9 @@
 #include <rte_ip6.h>
 
 enum {
-	IP_OUTPUT = 0,
-	CONTROL,
+	CONTROL = 0,
 	INVAL,
-	ERROR,
-	IGNORE,
+	DROP,
 	EDGE_COUNT,
 };
 
@@ -30,22 +28,15 @@ static uint16_t ndp_ns_input_process(
 	void **objs,
 	uint16_t nb_objs
 ) {
-	const struct nexthop *local, *remote;
+	struct control_output_mbuf_data *c;
 	struct icmp6_neigh_solicit *ns;
-	struct icmp6_neigh_advert *na;
-	struct ip6_local_mbuf_data *d;
-	struct rte_ipv6_addr src, dst;
+	struct ip6_local_mbuf_data d;
 	struct rte_ether_addr lladdr;
-	struct icmp6_opt_lladdr *ll;
-	const struct iface *iface;
-	struct rte_ipv6_hdr *ip;
-	struct icmp6_opt *opt;
+	const struct nexthop *local;
 	struct rte_mbuf *mbuf;
-	uint16_t payload_len;
 	struct icmp6 *icmp6;
 	bool lladdr_found;
 	rte_edge_t next;
-	bool solicited;
 
 #define ASSERT_NDP(condition)                                                                      \
 	do {                                                                                       \
@@ -58,127 +49,56 @@ static uint16_t ndp_ns_input_process(
 	for (uint16_t i = 0; i < nb_objs; i++) {
 		mbuf = objs[i];
 
-		d = ip6_local_mbuf_data(mbuf);
+		d = *ip6_local_mbuf_data(mbuf);
 		icmp6 = rte_pktmbuf_mtod(mbuf, struct icmp6 *);
 		ns = PAYLOAD(icmp6);
-		iface = d->iface;
-		src = d->src;
-		dst = d->dst;
 
 		// Validation of Neighbor Solicitations
 		// https://www.rfc-editor.org/rfc/rfc4861.html#section-7.1.1
 		//
 		// - The IP Hop Limit field has a value of 255, i.e., the packet
 		//   could not possibly have been forwarded by a router.
-		ASSERT_NDP(d->hop_limit == 255);
+		ASSERT_NDP(d.hop_limit == 255);
 		// - ICMP Checksum is valid. (already checked in icmp6_input)
 		//
 		// - ICMP Code is 0.
 		ASSERT_NDP(icmp6->code == 0);
 		// - ICMP length (derived from the IP length) is 24 or more octets.
-		ASSERT_NDP(d->len >= 0);
+		ASSERT_NDP(d.len >= 0);
 		// - Target Address is not a multicast address.
 		ASSERT_NDP(!rte_ipv6_addr_is_mcast(&ns->target));
 
-		local = ip6_nexthop_lookup(iface->vrf_id, iface->id, &ns->target);
+		local = ip6_nexthop_lookup(d.iface->vrf_id, d.iface->id, &ns->target);
 		if (local == NULL || !(local->flags & GR_NH_F_LOCAL)) {
-			next = IGNORE;
+			next = DROP;
 			if (gr_mbuf_is_traced(mbuf))
 				gr_mbuf_trace_add(mbuf, node, 0);
 			goto next;
 		}
 
-		lladdr_found = icmp6_get_opt(
-			mbuf, sizeof(*icmp6) + sizeof(*ns), ICMP6_OPT_SRC_LLADDR, &lladdr
-		);
-
-		if (rte_ipv6_addr_is_unspec(&src)) {
+		if (rte_ipv6_addr_is_unspec(&d.src)) {
 			// - If the IP source address is the unspecified address, the IP
 			//   destination address is a solicited-node multicast address.
-			ASSERT_NDP(rte_ipv6_addr_is_mcast(&dst));
+			ASSERT_NDP(rte_ipv6_addr_is_mcast(&d.dst));
 			// - If the IP source address is the unspecified address, there is
 			//   no source link-layer address option in the message.
+			lladdr_found = icmp6_get_opt(
+				mbuf, sizeof(*icmp6) + sizeof(*ns), ICMP6_OPT_SRC_LLADDR, &lladdr
+			);
 			ASSERT_NDP(!lladdr_found);
-			// If the source of the solicitation is the unspecified address, the
-			// node MUST set the Solicited flag to zero and multicast the
-			// advertisement to the all-nodes address.
-			src = (struct rte_ipv6_addr)RTE_IPV6_ADDR_ALLNODES_LINK_LOCAL;
-			solicited = false;
-			remote = NULL;
-		} else {
-			// Otherwise, the node MUST set the Solicited flag to one and unicast the
-			// advertisement to the Source Address of the solicitation.
-			solicited = true;
-			remote = ip6_nexthop_lookup(iface->vrf_id, iface->id, &src);
-
-			if (lladdr_found) {
-				// Copy the NS probe and send it to control plane for processing.
-				struct rte_mbuf *copy = rte_pktmbuf_copy(
-					mbuf, mbuf->pool, 0, UINT32_MAX
-				);
-				if (copy == NULL) {
-					next = ERROR;
-					if (gr_mbuf_is_traced(mbuf))
-						gr_mbuf_trace_add(mbuf, node, 0);
-					goto next;
-				}
-				if (gr_mbuf_is_traced(mbuf))
-					gr_mbuf_trace_add(copy, node, 0);
-
-				// HACK: replace the target IP with the *SOURCE* address of the NS
-				// sender. This removes the need to copy the whole IPv6 header for
-				// ndp_probe_input_cb.
-				ns = PAYLOAD(rte_pktmbuf_mtod(copy, struct icmp6 *));
-				ns->target = src;
-
-				struct control_output_mbuf_data *d;
-				d = control_output_mbuf_data(copy);
-				d->iface = iface;
-				d->callback = ndp_probe_input_cb;
-				rte_node_enqueue_x1(graph, node, CONTROL, copy);
-			}
 		}
 
-		// Reuse the mbuf to forge a neighbour advertisement reply.
-		// XXX,TODO: avoid issues with encap, remember previous data offset?
-		rte_pktmbuf_reset(mbuf);
-		icmp6 = (struct icmp6 *)rte_pktmbuf_append(mbuf, sizeof(*icmp6));
-		icmp6->type = ICMP6_TYPE_NEIGH_ADVERT;
-		icmp6->code = 0;
-		na = (struct icmp6_neigh_advert *)rte_pktmbuf_append(mbuf, sizeof(*na));
-		na->override = 1;
-		na->router = 1;
-		na->solicited = solicited;
-		na->target = local->ipv6;
-
-		opt = (struct icmp6_opt *)rte_pktmbuf_append(mbuf, sizeof(*opt));
-		opt->type = ICMP6_OPT_TARGET_LLADDR;
-		opt->len = ICMP6_OPT_LEN(sizeof(*opt) + sizeof(*ll));
-		ll = (struct icmp6_opt_lladdr *)rte_pktmbuf_append(mbuf, sizeof(*ll));
-		ll->mac = local->lladdr;
-
-		// ip6_output needs a nexthop to know the output interface. If we do not
-		// currently have a remote nexthop, set it to the interface that has the
-		// solicited address. If the source IP address is not unspecified AND
-		// a link-layer address option is present, the nexthop will be replaced
-		// with the remote one.
-		ip6_output_mbuf_data(mbuf)->nh = remote ?: local;
-
-		// Fill IPv6 layer
-		payload_len = rte_pktmbuf_pkt_len(mbuf);
-		ip = (struct rte_ipv6_hdr *)rte_pktmbuf_prepend(mbuf, sizeof(*ip));
-		ip6_set_fields(ip, payload_len, IPPROTO_ICMPV6, &na->target, &src);
-		// Compute ICMP6 checksum with pseudo header
-		icmp6->cksum = 0;
-		icmp6->cksum = rte_ipv6_udptcp_cksum(ip, icmp6);
-
+		c = control_output_mbuf_data(mbuf);
+		c->iface = d.iface;
+		c->callback = ndp_probe_input_cb;
+		memcpy(c->cb_data, &d, sizeof(d));
+		next = CONTROL;
+next:
 		if (gr_mbuf_is_traced(mbuf)) {
-			uint8_t trace_len = RTE_MIN(payload_len, GR_TRACE_ITEM_MAX_LEN);
+			uint8_t trace_len = RTE_MIN(d.len, GR_TRACE_ITEM_MAX_LEN);
 			struct icmp6 *t = gr_mbuf_trace_add(mbuf, node, trace_len);
 			memcpy(t, icmp6, trace_len);
 		}
-		next = IP_OUTPUT;
-next:
 		rte_node_enqueue_x1(graph, node, next, mbuf);
 	}
 
@@ -192,11 +112,9 @@ static struct rte_node_register node = {
 
 	.nb_edges = EDGE_COUNT,
 	.next_nodes = {
-		[IP_OUTPUT] = "ip6_output",
 		[CONTROL] = "control_output",
 		[INVAL] = "ndp_ns_input_inval",
-		[ERROR] = "ndp_ns_input_error",
-		[IGNORE] = "ndp_ns_input_ignore",
+		[DROP] = "ndp_ns_input_drop",
 	},
 };
 
@@ -208,5 +126,4 @@ static struct gr_node_info info = {
 GR_NODE_REGISTER(info);
 
 GR_DROP_REGISTER(ndp_ns_input_inval);
-GR_DROP_REGISTER(ndp_ns_input_error);
-GR_DROP_REGISTER(ndp_ns_input_ignore);
+GR_DROP_REGISTER(ndp_ns_input_drop);

modules/ip6/control/nexthop.c
@@ -111,29 +111,35 @@ free:
 	rte_pktmbuf_free(m);
 }
 
+static control_input_t ndp_na_output_node;
+
 void ndp_probe_input_cb(struct rte_mbuf *m) {
 	const struct icmp6 *icmp6 = rte_pktmbuf_mtod(m, const struct icmp6 *);
-	const struct iface *iface = mbuf_data(m)->iface;
+	const struct rte_ipv6_addr *remote, *local;
+	const struct ip6_local_mbuf_data *d;
 	const struct icmp6_neigh_solicit *ns;
 	const struct icmp6_neigh_advert *na;
-	const struct rte_ipv6_addr *target;
+	const struct iface *iface;
 	struct rte_ether_addr mac;
-	struct nexthop *nh;
+	struct nexthop *nh = NULL;
 	bool lladdr_found;
+
+	d = (const struct ip6_local_mbuf_data *)control_output_mbuf_data(m)->cb_data;
+	iface = control_output_mbuf_data(m)->iface;
 
 	switch (icmp6->type) {
 	case ICMP6_TYPE_NEIGH_SOLICIT:
 		ns = PAYLOAD(icmp6);
-		// HACK: the target IP contains the *SOURCE* address of the NS sender. It was
-		// replaced in ndp_ns_input_process to avoid copying the whole IPv6 header.
-		target = &ns->target;
+		local = &ns->target;
+		remote = &d->src;
 		lladdr_found = icmp6_get_opt(
 			m, sizeof(*icmp6) + sizeof(*ns), ICMP6_OPT_SRC_LLADDR, &mac
 		);
 		break;
 	case ICMP6_TYPE_NEIGH_ADVERT:
 		na = PAYLOAD(icmp6);
-		target = &na->target;
+		local = NULL;
+		remote = &na->target;
 		lladdr_found = icmp6_get_opt(
 			m, sizeof(*icmp6) + sizeof(*na), ICMP6_OPT_TARGET_LLADDR, &mac
 		);
@@ -141,41 +147,55 @@ void ndp_probe_input_cb(struct rte_mbuf *m) {
 	default:
 		goto free;
 	}
-	if (!lladdr_found)
-		goto free;
 
-	nh = ip6_nexthop_lookup(iface->vrf_id, iface->id, target);
-	if (nh == NULL) {
-		// We don't have an entry for the probe sender address yet.
-		//
-		// Create one now. If the sender has requested our mac address, they
-		// will certainly contact us soon and it will save us an NDP solicitation.
-		if ((nh = ip6_nexthop_new(iface->vrf_id, iface->id, target)) == NULL) {
-			LOG(ERR, "ip6_nexthop_new: %s", strerror(errno));
-			goto free;
-		}
+	if (!rte_ipv6_addr_is_unspec(remote) && !rte_ipv6_addr_is_mcast(remote)) {
+		nh = ip6_nexthop_lookup(iface->vrf_id, iface->id, remote);
+		if (nh == NULL) {
+			// We don't have an entry for the probe sender address yet.
+			//
+			// Create one now. If the sender has requested our mac address, they
+			// will certainly contact us soon and it will save us an NDP solicitation.
+			if ((nh = ip6_nexthop_new(iface->vrf_id, iface->id, remote)) == NULL) {
+				LOG(ERR, "ip6_nexthop_new: %s", strerror(errno));
+				goto free;
+			}
 
-		// Add an internal /128 route to reference the newly created nexthop.
-		if (ip6_route_insert(iface->vrf_id, iface->id, target, RTE_IPV6_MAX_DEPTH, nh)
-		    < 0) {
-			LOG(ERR, "ip6_route_insert: %s", strerror(errno));
-			goto free;
+			// Add an internal /128 route to reference the newly created nexthop.
+			if (ip6_route_insert(
+				    iface->vrf_id, iface->id, remote, RTE_IPV6_MAX_DEPTH, nh
+			    )
+			    < 0) {
+				LOG(ERR, "ip6_route_insert: %s", strerror(errno));
+				goto free;
+			}
 		}
 	}
 
-	// Static next hops never need updating.
-	if (nh->flags & GR_NH_F_STATIC)
-		goto free;
+	if (nh && !(nh->flags & GR_NH_F_STATIC) && lladdr_found) {
+		// Refresh all fields.
+		nh->last_reply = rte_get_tsc_cycles();
+		nh->iface_id = iface->id;
+		nh->flags |= GR_NH_F_REACHABLE;
+		nh->flags &= ~(GR_NH_F_STALE | GR_NH_F_PENDING | GR_NH_F_FAILED);
+		nh->ucast_probes = 0;
+		nh->bcast_probes = 0;
+		nh->lladdr = mac;
+		nexthop_push_notification(NEXTHOP_EVENT_UPDATE, nh);
+	}
 
-	// Refresh all fields.
-	nh->last_reply = rte_get_tsc_cycles();
-	nh->iface_id = iface->id;
-	nh->flags |= GR_NH_F_REACHABLE;
-	nh->flags &= ~(GR_NH_F_STALE | GR_NH_F_PENDING | GR_NH_F_FAILED);
-	nh->ucast_probes = 0;
-	nh->bcast_probes = 0;
-	nh->lladdr = mac;
-	nexthop_push_notification(NEXTHOP_EVENT_UPDATE, nh);
+	if (icmp6->type == ICMP6_TYPE_NEIGH_SOLICIT && local != NULL) {
+		// send a reply for our local ip
+		struct ndp_na_output_mbuf_data *d = ndp_na_output_mbuf_data(m);
+		d->local = ip6_nexthop_lookup(iface->vrf_id, iface->id, local);
+		d->remote = nh;
+		d->iface = iface;
+		if (post_to_stack(ndp_na_output_node, m) < 0) {
+			LOG(ERR, "post_to_stack: %s", strerror(errno));
+			goto free;
+		}
+		// prevent double free, mbuf has been re-consumed by datapath
+		m = NULL;
+	}
 
 	// Flush all held packets.
 	struct rte_mbuf *held = nh->held_pkts_head;
@@ -309,6 +329,7 @@ static void nh6_init(struct event_base *ev_base) {
 		ABORT("nh_pool_new(AF_INET6) failed");
 
 	ip6_output_node = gr_control_input_register_handler("ip6_output", true);
+	ndp_na_output_node = gr_control_input_register_handler("ndp_na_output", true);
 }
 
 static void nh6_fini(struct event_base *) {

modules/ip6/datapath/gr_ip6_datapath.h
@@ -28,6 +28,11 @@ GR_MBUF_PRIV_DATA_TYPE(ip6_local_mbuf_data, {
 	uint8_t proto;
 });
 
+GR_MBUF_PRIV_DATA_TYPE(ndp_na_output_mbuf_data, {
+	const struct nexthop *local;
+	const struct nexthop *remote;
+});
+
 void ip6_input_local_add_proto(uint8_t proto, const char *next_node);
 void ip6_output_register_interface(uint16_t iface_type_id, const char *next_node);
 int ip6_nexthop_solicit(struct nexthop *nh);

modules/ip6/datapath/ndp_na_output.c
@@ -1,0 +1,105 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#include <gr_control_input.h>
+#include <gr_graph.h>
+#include <gr_icmp6.h>
+#include <gr_ip6_control.h>
+#include <gr_ip6_datapath.h>
+#include <gr_log.h>
+#include <gr_mbuf.h>
+#include <gr_trace.h>
+
+#include <rte_byteorder.h>
+#include <rte_ether.h>
+#include <rte_graph_worker.h>
+#include <rte_ip6.h>
+
+enum {
+	OUTPUT = 0,
+	EDGE_COUNT,
+};
+
+static uint16_t ndp_na_output_process(
+	struct rte_graph *graph,
+	struct rte_node *node,
+	void **objs,
+	uint16_t nb_objs
+) {
+	const struct nexthop *local, *remote;
+	struct ip6_local_mbuf_data *d;
+	struct icmp6_neigh_advert *na;
+	struct icmp6_opt_lladdr *ll;
+	struct icmp6_opt *opt;
+	struct rte_mbuf *mbuf;
+	uint16_t payload_len;
+	struct icmp6 *icmp6;
+
+	for (uint16_t i = 0; i < nb_objs; i++) {
+		mbuf = objs[i];
+
+		local = ndp_na_output_mbuf_data(mbuf)->local;
+		remote = ndp_na_output_mbuf_data(mbuf)->remote;
+
+		rte_pktmbuf_trim(mbuf, rte_pktmbuf_pkt_len(mbuf));
+
+		// Fill ICMP6 layer.
+		payload_len = sizeof(*icmp6) + sizeof(*na) + sizeof(*opt) + sizeof(*ll);
+		icmp6 = (struct icmp6 *)rte_pktmbuf_append(mbuf, payload_len);
+		icmp6->type = ICMP6_TYPE_NEIGH_ADVERT;
+		icmp6->code = 0;
+		na = PAYLOAD(icmp6);
+		na->override = 1;
+		na->router = 1;
+		na->solicited = remote != NULL;
+		na->target = local->ipv6;
+		opt = PAYLOAD(na);
+		opt->type = ICMP6_OPT_TARGET_LLADDR;
+		opt->len = ICMP6_OPT_LEN(sizeof(*opt) + sizeof(*ll));
+		ll = PAYLOAD(opt);
+		ll->mac = local->lladdr;
+
+		// Fill in IP local data
+		d = ip6_local_mbuf_data(mbuf);
+		d->iface = iface_from_id(local->iface_id);
+		d->src = local->ipv6;
+		if (remote == NULL) {
+			// If the source of the solicitation is the unspecified address, the
+			// node MUST set the Solicited flag to zero and multicast the
+			// advertisement to the all-nodes address.
+			d->dst = (struct rte_ipv6_addr)RTE_IPV6_ADDR_ALLNODES_LINK_LOCAL;
+		} else {
+			d->dst = remote->ipv6;
+		}
+		d->len = payload_len;
+		d->hop_limit = IP6_DEFAULT_HOP_LIMIT;
+		d->proto = IPPROTO_ICMPV6;
+
+		if (gr_mbuf_is_traced(mbuf)) {
+			uint8_t trace_len = RTE_MIN(payload_len, GR_TRACE_ITEM_MAX_LEN);
+			struct icmp6 *t = gr_mbuf_trace_add(mbuf, node, trace_len);
+			memcpy(t, icmp6, trace_len);
+		}
+		rte_node_enqueue_x1(graph, node, OUTPUT, mbuf);
+	}
+
+	return nb_objs;
+}
+
+static struct rte_node_register node = {
+	.name = "ndp_na_output",
+
+	.process = ndp_na_output_process,
+
+	.nb_edges = EDGE_COUNT,
+	.next_nodes = {
+		[OUTPUT] = "icmp6_output",
+	},
+};
+
+static struct gr_node_info info = {
+	.node = &node,
+	.trace_format = (gr_trace_format_cb_t)trace_icmp6_format,
+};
+
+GR_NODE_REGISTER(info);

commit 25db38458bb317a95982cd9c41d79c14da633601

ip6: require a full neighbour solicitation before reading its target
ndp_ns_input asserts d.len >= 0 where the comment right above it, and RFC
4861 section 7.1.1, ask for 24 octets. len is a uint16_t so the condition
never fires. An eight byte solicitation with a valid checksum passes the
generic length check in icmp6_input, reaches this node, and the target
address is read from beyond the announced payload.

The three sibling nodes get it right: ndp_na_input checks 24, ndp_ra_input
and ndp_rs_input size their own message. Check the same way here.

Fixes: b51dfd92f09f ("ndp: avoid races between control and data planes")
Signed-off-by: Maxime Leroy <maxime@leroys.fr>

modules/ip6/datapath/meson.build
@@ -30,6 +30,10 @@ tests += [
     'link_args': [],
   },
   {
+    'sources': files('ndp_ns_input.c'),
+    'link_args': [],
+  },
+  {
     'sources': files('ip6_local.c'),
     'link_args': [],
   },

modules/ip6/datapath/ndp_ns_input.c
@@ -61,7 +61,7 @@ static uint16_t ndp_ns_input_process(
 		// - ICMP Code is 0.
 		ASSERT_NDP(icmp6->code == 0);
 		// - ICMP length (derived from the IP length) is 24 or more octets.
-		ASSERT_NDP(d.len >= 0);
+		ASSERT_NDP(d.len >= sizeof(*icmp6) + sizeof(*ns));
 		// - Target Address is not a multicast address.
 		ASSERT_NDP(!rte_ipv6_addr_is_mcast(&ns->target));
 
@@ -125,3 +125,77 @@ GR_NODE_REGISTER(info);
 
 GR_DROP_REGISTER(ndp_ns_input_inval);
 GR_DROP_REGISTER(ndp_ns_input_drop);
+
+#ifdef __GROUT_UNIT_TEST__
+
+#include "_cmocka.h"
+
+struct node_infos node_infos = STAILQ_HEAD_INITIALIZER(node_infos);
+
+int cq_callback_offset;
+int cq_priv_offset;
+mock_func(uint16_t, drop_packets(struct rte_graph *, struct rte_node *, void **, uint16_t));
+mock_func(int, drop_format(char *, size_t, const void *, size_t));
+mock_func(void *, gr_mbuf_trace_add(struct rte_mbuf *, struct rte_node *, size_t));
+mock_func(int, trace_icmp6_format(char *, size_t, const struct icmp6 *, size_t));
+mock_func(struct nexthop *, nexthop_lookup_l3(addr_family_t, uint16_t, uint16_t, const void *));
+mock_func(void, ndp_probe_input_cb(void *, uintptr_t, const struct control_queue_drain *));
+
+struct fake_ndp_ns_mbuf {
+	struct icmp6 icmp6_hdr;
+	struct icmp6_neigh_solicit ns_hdr;
+	struct rte_mbuf mbuf;
+	uint8_t priv_data[GR_MBUF_PRIV_MAX_SIZE];
+};
+
+static struct iface test_iface;
+
+static void init_default_ns_mbuf(struct fake_ndp_ns_mbuf *ndp_mbuf) {
+	memset(ndp_mbuf, 0, sizeof(*ndp_mbuf));
+
+	ndp_mbuf->icmp6_hdr.type = ICMP6_TYPE_NEIGH_SOLICIT;
+	ndp_mbuf->icmp6_hdr.code = 0;
+	ndp_mbuf->ns_hdr.target = (struct rte_ipv6_addr)RTE_IPV6(
+		0xfe80, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x00aa
+	);
+
+	ndp_mbuf->mbuf.buf_addr = &ndp_mbuf->icmp6_hdr;
+	ndp_mbuf->mbuf.data_len = sizeof(struct icmp6) + sizeof(struct icmp6_neigh_solicit);
+	ndp_mbuf->mbuf.pkt_len = ndp_mbuf->mbuf.data_len;
+	ndp_mbuf->mbuf.next = NULL;
+	ndp_mbuf->mbuf.ol_flags = 0;
+	ndp_mbuf->mbuf.packet_type = RTE_PTYPE_L4_ICMP;
+
+	ip6_local_mbuf_data(&ndp_mbuf->mbuf)->hop_limit = 255;
+	ip6_local_mbuf_data(&ndp_mbuf->mbuf)->len = ndp_mbuf->mbuf.data_len;
+	ip6_local_mbuf_data(&ndp_mbuf->mbuf)->src = (struct rte_ipv6_addr)RTE_IPV6(
+		0xfe80, 0, 0, 0, 0, 0, 0, 0x00bb
+	);
+	ip6_local_mbuf_data(&ndp_mbuf->mbuf)->dst = (struct rte_ipv6_addr)RTE_IPV6(
+		0xfe80, 0, 0, 0, 0, 0, 0, 0x00aa
+	);
+	ip6_local_mbuf_data(&ndp_mbuf->mbuf)->iface = &test_iface;
+}
+
+// A solicitation carrying only the generic ICMPv6 header has no target address
+// to read. RFC 4861 requires 24 octets.
+static void ndp_ns_input_icmp_len_invalid(void **) {
+	struct fake_ndp_ns_mbuf ndp_mbuf;
+	void *obj = &ndp_mbuf.mbuf;
+
+	init_default_ns_mbuf(&ndp_mbuf);
+	ip6_local_mbuf_data(obj)->len = GR_ICMP6_HDR_LEN;
+
+	expect_uint_value(rte_node_enqueue_x1, next, INVAL);
+
+	ndp_ns_input_process(NULL, NULL, &obj, 1);
+}
+
+int main(void) {
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(ndp_ns_input_icmp_len_invalid),
+	};
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
+
+#endif